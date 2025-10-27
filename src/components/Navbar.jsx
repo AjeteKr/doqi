cebf9306f3049750aa73e6005b4af459082d95f7
Ajete Krasniqi <ajetekr@gmail.com>
2025-10-27 10:33:29 +0100

    fix: fixing the logo of Doqi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
             <Link to="/" className="flex items-center">
               <div className="flex items-center space-x-2">
                 <img 
-                  src="/src/assets/images/doqi-logo.png" 
-                  alt="DOQI Logo" 
+                  src="/images/doqi-logo.png" 
+                  alt="DoqiSHPK Logo" 
                   className="h-10 w-auto"
                 />
               </div>
